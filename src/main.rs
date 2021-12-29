b440ce6378d7b4d8a2abc5c2a8b9e8780e80f8a7
beProsto <lavionperavion2@gmail.com>
2021-12-29 21:19:48 +0100

    reworked to work with transmitters and recievers

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@
 
 // c++'s "using"
 use rand::random as rand; // shortens rand::random to rand
-use std::sync::{Arc, Mutex}; // shortens std::sync::Arc and Mutex to just Arc and Mutex
-use std::{thread, time}; // shortens std::thread and time to just thread and time
+use std::sync::mpsc; // shortens std::sync::mpsc to mpsc
+use std::{thread, time}; // shortens std::thread and time to just thread and time // shortens std::mem to mem
 
 // Width and Height of the square in which we'll draw
 const WIDTH: i32 = 42;
@@ -24,7 +24,6 @@ struct ObjectData {
 
 // This will contain the data that will be used in the loop
 struct GameData {
-    running: bool,
     pressed: char,
     player: ObjectData,
     falling_object: ObjectData,
@@ -46,8 +45,7 @@ fn render(data: &mut GameData, rendercharacter: &dyn Fn(&mut GameData, i32, i32)
 
 fn main() {
     // We define the data
-    let gamedata_arc = Arc::new(Mutex::new(GameData {
-        running: true,
+    let mut gamedata = GameData {
         pressed: '\0',
         player: ObjectData {
             x: 20.0,
@@ -59,9 +57,9 @@ fn main() {
             y: -4.0,
             character: 'V',
         },
-    }));
+    };
 
-    let gamedata_arc_input = Arc::clone(&gamedata_arc);
+    let (input_tx, input_rx) = mpsc::channel();
 
     // Clear the screen
     print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
@@ -74,83 +72,77 @@ fn main() {
             // This loop is frozen up until we click anything,
             // that's why it's on a seperate thread from the rendering!
             let character = winconsole::console::getch(true).unwrap();
-            /* mutex lock scope */
-            {
-                // We get game data's reference
-                let mut gamedata_ref = gamedata_arc_input.lock().unwrap();
-                // We set the current pressed character to, well, the one that is pressed
-                gamedata_ref.pressed = character;
-                // Not necessary but I'd like to know what's pressed ^^
-                println!("Getch ret: '{}' !!!", gamedata_ref.pressed);
-                // If the currently pressed button is Enter, we break out of the loop (on both threads)
-                if gamedata_ref.pressed == '\r' {
-                    gamedata_ref.running = false;
-                    break;
-                }
+            // We send the gotten character to the main thread
+            input_tx.send(character).unwrap();
+            // If the currently pressed button is Enter, we break out of the loop
+            if character == '\r' {
+                break;
             }
-            thread::sleep(time::Duration::from_millis(10));
         }
     });
 
     // The main loop
     loop {
-        /* mutex lock scope */
-        {
-            // We get the reference to the game data.
-            let mut gamedata_ref = gamedata_arc.lock().unwrap();
-
-            // If we broke out of the loop on the other thread - we do so on this one.
-            if !gamedata_ref.running {
-                break;
-            }
-
-            // Player movement
-            if gamedata_ref.pressed == 'd' {
-                gamedata_ref.player.x += 1.0;
-            } else if gamedata_ref.pressed == 'a' {
-                gamedata_ref.player.x -= 1.0;
-            } else if gamedata_ref.pressed == 's' {
-                gamedata_ref.player.y += 1.0;
-            } else if gamedata_ref.pressed == 'w' {
-                gamedata_ref.player.y -= 1.0;
-            }
-
-            // Falling object fall
-            gamedata_ref.falling_object.y += 0.1;
-            // Falling object crawling back to the top
-            if gamedata_ref.falling_object.y as i32 >= HEIGHT {
-                gamedata_ref.falling_object.y = -1.0;
-                gamedata_ref.falling_object.x = (rand::<i32>() % (WIDTH - 2) + 1) as f32;
-            }
-
-            // We render to the area
-            render(
-                &mut gamedata_ref,
-                // This function happens per every character to be rendered
-                &|data: &mut GameData, y: i32, x: i32| {
-                    // Border around the "screen"
-                    if x == 0 || x == WIDTH - 1 || y == 0 || y == HEIGHT - 1 {
-                        print!("=");
-                    }
-                    // Rendering the player
-                    else if data.player.x as i32 == x && data.player.y as i32 == y {
-                        print!("{}", data.player.character);
-                    }
-                    // Rendering the falling object
-                    else if data.falling_object.x as i32 == x && data.falling_object.y as i32 == y
-                    {
-                        print!("{}", data.falling_object.character);
-                    }
-                    // The blank space
-                    else {
-                        print!(" ");
-                    }
-                },
-            );
-
-            // We reset the character pressed, in order not to preserve it when it's not clicked
-            gamedata_ref.pressed = '\0';
+        // Recieve the character (if there is any to recieve)
+        let recieved = input_rx.try_recv();
+        if !recieved.is_err() {
+            gamedata.pressed = recieved.unwrap();
         }
+
+        // If we broke out of the loop on the other thread - we do so on this one.
+        if gamedata.pressed == '\r' {
+            break;
+        }
+
+        // Player movement
+        if gamedata.pressed == 'd' {
+            gamedata.player.x += 1.0;
+        } else if gamedata.pressed == 'a' {
+            gamedata.player.x -= 1.0;
+        } else if gamedata.pressed == 's' {
+            gamedata.player.y += 1.0;
+        } else if gamedata.pressed == 'w' {
+            gamedata.player.y -= 1.0;
+        }
+
+        // Falling object fall
+        gamedata.falling_object.y += 0.1;
+        // Falling object crawling back to the top
+        if gamedata.falling_object.y as i32 >= HEIGHT {
+            gamedata.falling_object.y = -1.0;
+            gamedata.falling_object.x = (rand::<i32>() % (WIDTH - 2) + 1) as f32;
+        }
+
+        // We render to the area
+        render(
+            &mut gamedata,
+            // This function happens per every character to be rendered
+            &|data: &mut GameData, y: i32, x: i32| {
+                // Border around the "screen"
+                if x == 0 || x == WIDTH - 1 || y == 0 || y == HEIGHT - 1 {
+                    print!("=");
+                }
+                // Rendering the player
+                else if data.player.x as i32 == x && data.player.y as i32 == y {
+                    print!("{}", data.player.character);
+                }
+                // Rendering the falling object
+                else if data.falling_object.x as i32 == x && data.falling_object.y as i32 == y {
+                    print!("{}", data.falling_object.character);
+                }
+                // The blank space
+                else {
+                    print!(" ");
+                }
+            },
+        );
+
+		// Not necessary but I'd like to know what's pressed ^^
+		println!("Getch ret: '{}' !!!", gamedata.pressed);
+
+        // We reset the character pressed, in order not to preserve it when it's not clicked
+        gamedata.pressed = '\0';
+
         // Makes this thread sleep without a locked mutex, so the other one can catch up and do it's thing (only if it needs to) :D
         thread::sleep(time::Duration::from_millis(10));
     }
